f36977ded0be752980765ebe9fa4dd0745f615fe
DESKTOP-419VCQ8\AceKnight <triettruong@itrvn.com>
2021-11-29 15:21:15 +0700

    custom home-body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { TestComponent } from './test/test.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TestComponent } from './pages/test/test.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent, HomeComponent, TestComponent],
-  imports: [BrowserModule, NzButtonModule, MatButtonModule, MatIconModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    NzButtonModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
