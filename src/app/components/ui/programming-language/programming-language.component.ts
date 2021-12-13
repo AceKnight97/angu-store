6203cadda267ba8a34e4dcf6fcea7178eefa3ec0
DESKTOP-419VCQ8\AceKnight <triettruong@itrvn.com>
2021-12-13 11:11:10 +0700

    trying to add more row

--- src/app/components/ui/programming-language/programming-language.component.ts
+++ src/app/components/ui/programming-language/programming-language.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-programming-language',
   templateUrl: './programming-language.component.html',
-  styleUrls: ['./programming-language.component.css'],
+  styleUrls: ['./programming-language.component.scss'],
 })
 export class ProgrammingLanguageComponent implements OnInit {
-  @Input() technicalSkillset: any = '';
-  @Input() competence: any = '';
-  @Input() level: any = '';
-
-  angForm = new FormGroup({
-    technicalSkillset: new FormControl(this.technicalSkillset),
-    competence: new FormControl(this.competence),
-    level: new FormControl(this.level),
+  @Input() programingLanguage: any = new FormGroup({
+    technicalSkillset: new FormControl(''),
+    competence: new FormControl(''),
+    level: new FormControl(''),
   });
+  @Input() index: number = 0;
+  @Input() onClickAddPL: any = () => {};
+  @Output() onClickAddPLChange = new EventEmitter<any>();
+
   constructor() {}
+  ngOnChanges(): void {
+    // console.log('object: ', this.onClickAddPL);
+    this.onClickAddPLChange.emit(this.onClickAddPL);
+  }
 
   ngOnInit(): void {}
+  onFormSubmit(): void {}
+  onClickAdd(): void {
+    // console.log({ index: this.index, asdas: this.onClickAddPL });
+    this.onClickAddPL(this.index);
+  }
+  onClickRemove(): void {}
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Home } from './home';
-import { FormControl, Validators, FormGroup, FormArray } from '@angular/forms';
+import {
+  FormControl,
+  Validators,
+  FormGroup,
+  FormArray,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-pages-home',
@@ -8,59 +14,61 @@ import { FormControl, Validators, FormGroup, FormArray } from '@angular/forms';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  angForm = new FormGroup({
-    // HEADER
-    photo: new FormControl('test_photo_link'), // , Validators.required
-    username: new FormControl('Truong Thanh Triet'),
-    jobTitle: new FormControl('Front-end developer'),
-    // PERSONAL INFO
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    address: new FormControl(''),
-    gender: new FormControl(''),
-    dob: new FormControl(''),
-    linkedin: new FormControl(''),
-    careerObjective: new FormControl(''),
-    // PERSONAL INFO
-    programmingLanguages: new FormArray([
-      new FormControl('Mahesh'),
-      new FormControl('Krishna'),
-    ]),
-    // : new FormControl(''),
-    // : new FormControl(''),
-    // : new FormControl(''),
-    // : new FormControl(''),
-    // : new FormControl(''),
+  proLanguage = new FormGroup({
+    technicalSkillset: new FormControl('a'),
+    competence: new FormControl('b'),
+    level: new FormControl('c'),
   });
-  constructor() {}
+  // angForm = new FormGroup({
+  //   // HEADER
+  //   photo: new FormControl('test_photo_link'), // , Validators.required
+  //   username: new FormControl('Truong Thanh Triet'),
+  //   jobTitle: new FormControl('Front-end developer'),
+  //   // PERSONAL INFO
+  //   email: new FormControl(''),
+  //   phone: new FormControl(''),
+  //   address: new FormControl(''),
+  //   gender: new FormControl(''),
+  //   dob: new FormControl(''),
+  //   linkedin: new FormControl(''),
+  //   careerObjective: new FormControl(''),
+  //   // PERSONAL INFO
+  //   programmingLanguages: new FormArray([this.proLanguage]),
+  //   // : new FormControl(''),
+  // });
+  angForm;
+  constructor(private fb: FormBuilder) {
+    this.angForm = this.fb.group({
+      photo: new FormControl('test_photo_link'),
+      username: new FormControl('Truong Thanh Triet'),
+      jobTitle: new FormControl('Front-end developer'),
+      // PERSONAL INFO
+      email: new FormControl(''),
+      phone: new FormControl(''),
+      address: new FormControl(''),
+      gender: new FormControl(''),
+      dob: new FormControl(''),
+      linkedin: new FormControl(''),
+      careerObjective: new FormControl(''),
+      // PERSONAL INFO
+      programmingLanguages: this.fb.array([this.proLanguage]),
+    });
+  }
   // homeModel = new Home('', '');
   ngOnInit(): void {}
 
-  get name(): any {
-    return this.angForm.get('name');
-  }
-  get age(): any {
-    return this.angForm.get('age');
-  }
-  get college(): any {
-    return this.angForm.get('college');
-  }
+  // get name(): any {
+  //   return this.angForm.get('name');
+  // }
   get programmingLanguages() {
     return this.angForm.get('programmingLanguages') as FormArray;
   }
 
   onFormSubmit(): void {
-    console.log(
-      'Name: ' + this.angForm.get('name')?.value + ' =',
-      this.name.value
-    );
-    console.log(
-      'Age: ' + this.angForm.get('age')?.value + ' =',
-      this.age.value
-    );
-    console.log(
-      'College: ' + this.angForm.get('college')?.value + ' =',
-      this.college.value
-    );
+    console.log({ res: this.angForm.value });
+  }
+  onClickAddPL(index: number = 0): void {
+    const test = this.programmingLanguages;
+    console.log({ test });
   }
 }
